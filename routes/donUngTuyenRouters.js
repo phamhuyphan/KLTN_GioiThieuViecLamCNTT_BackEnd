354469014c0fb0737853ecc2118b311e3e020bfa
trankhoadev <trankhoadev@gmail.com>
2023-05-12 02:07:02 +0700

    fix lại export ra tên hàm để sử dụng

--- routes/donUngTuyenRouters.js
+++ routes/donUngTuyenRouters.js
@@ -4,7 +4,8 @@ const {
     createDonUngTuyen,
     deleteDonUngTuyen,
     updateDonUngTuyen,
-    getAllDonUngTuyenByTinTuyenDung,getAllDonUngTuyenByUngTuyenVien
+    getAllDonUngTuyenByTinTuyenDung,getAllDonUngTuyenByUngTuyenVien,
+    getAllDonUngTuyenByNhaTuyenDung
   } = require("../controllers/DonUngTuyenController")
   const { protect } = require("../middlewares/authMiddleware")
 
@@ -13,7 +14,7 @@ const {
 router.route("/").get(protect,accessDonUngTuyen);
 router.route("/:ungtuyenvienId").get(protect,getAllDonUngTuyenByUngTuyenVien);
 router.route("/tintuyendung/:tintuyendungId").get(protect,getAllDonUngTuyenByTinTuyenDung);
-router.route("/nhatuyendung/:nhaTuyenDungId").get(protect,getAllDonUngTuyenByTinTuyenDung);
+router.route("/nhatuyendung/:nhaTuyenDungId").get(protect,getAllDonUngTuyenByNhaTuyenDung);
 router.route("/").post(protect,createDonUngTuyen);
 router.route("/update").put(protect,updateDonUngTuyen);
 router.route("/").delete(protect,deleteDonUngTuyen);
